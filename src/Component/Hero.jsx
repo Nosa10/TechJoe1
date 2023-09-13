8102d3253a29761c344c4d528d24342a90868a71
Nosa10 <edisonnosakhare@gmail.com>
2023-09-13 05:45:30 -0700

    Changes

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
   return (
     <Container className ="hero" id='hero'>
       <Row>
-        <Col xs={12} md={6}>
+        <Col xs={12} md={8}>
           <h2>The Tale of Uzumaki Naruto</h2>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
             Modi, dolores itaque fugiat voluptatibus libero harum ex 
@@ -24,8 +24,32 @@ function Hero() {
             Modi, dolores itaque fugiat voluptatibus libero harum ex 
             impedit voluptas ducimus debitis hic assumenda magni? 
             Consequatur temporibus ab repudiandae sapiente non sint!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Modi, dolores itaque fugiat voluptatibus libero harum ex 
+            impedit voluptas ducimus debitis hic assumenda magni? 
+            Consequatur temporibus ab repudiandae sapiente non sint!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Modi, dolores itaque fugiat voluptatibus libero harum ex 
+            impedit voluptas ducimus debitis hic assumenda magni? 
+            Consequatur temporibus ab repudiandae sapiente non sint!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Modi, dolores itaque fugiat voluptatibus libero harum ex 
+            impedit voluptas ducimus debitis hic assumenda magni? 
+            Consequatur temporibus ab repudiandae sapiente non sint!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Modi, dolores itaque fugiat voluptatibus libero harum ex 
+            impedit voluptas ducimus debitis hic assumenda magni? 
+            Consequatur temporibus ab repudiandae sapiente non sint!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Modi, dolores itaque fugiat voluptatibus libero harum ex 
+            impedit voluptas ducimus debitis hic assumenda magni? 
+            Consequatur temporibus ab repudiandae sapiente non sint!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Modi, dolores itaque fugiat voluptatibus libero harum ex 
+            impedit voluptas ducimus debitis hic assumenda magni? 
+            Consequatur temporibus ab repudiandae sapiente non sint!</p>
         </Col>
-        <Col xs={12} md={6}>
+        <Col xs={12} md={4}>
           <img src="NineTailFox-transformed.png"></img>
         </Col>
       </Row>
